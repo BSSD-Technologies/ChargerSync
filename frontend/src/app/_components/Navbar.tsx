cc9b5f3c0f4e06480ae7d8b207dd32e4f69b297a
Salwa Jeries <annette.jeries@gmail.com>
2024-02-19 17:54:02 -0600

    Rearrange for navbar

---
+++ frontend/src/app/_components/Navbar.tsx
@@ -0,0 +1,107 @@
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import { useState } from "react";
+import Image from "next/image";
+import WhiteLogo from "@/app/_assets/WhiteLogo.svg";
+
+const pages = ["Generate Schedule", "Help Manual"];
+
+export default function Navbar() {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            component="a"
+            href=""
+            sx={{
+              mr: 3,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            <Image src={WhiteLogo} alt={"Charger Sync"} height={60}></Image>
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuRoundedIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+            }}
+          >
+            <Image src={WhiteLogo} alt={"Charger Sync"} height={60}></Image>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Typography } from "@mui/material";
+import Navbar from "./_components/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Typography variant="h1">Whereas disregard and contempt for human rights have resulted</Typography>
       <Typography variant="body1">Testing one two three</Typography>
     </main>
