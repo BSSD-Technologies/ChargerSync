7a59b65a377b4f948a44735ec00e0c508f0c58cc
thebamorgan <77521759+thebamorgan@users.noreply.github.com>
2024-02-28 14:26:15 -0600

    Created backend/dummyData

--- backend/templateReader.py
+++ backend/templateReader.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import sqlalchemy as magic
+from datetime import time
+import os
 
+
+# isFile = os.path.isfile(fpath)
+
+# print(isFile)
 # df = pd.read_excel("Template.xlsx", sheet_name=None)
 
-courses = pd.read_excel('Template.xlsx', sheet_name='Courses')
-classrooms = pd.read_excel('Template.xlsx', sheet_name='Classrooms')
-timeSlots = pd.read_excel('Template.xlsx', sheet_name='Time')
-professors = pd.read_excel('Template.xlsx', sheet_name='Professors')
+courseList = pd.read_csv("./dummyData/CourseListTemplate.csv", dtype={0:'string', 1:'int', 2:'int'})
 
-print(courses.items())
+course_ID = courseList['Course ID']
+course_Max = courseList['Max Enrollment']
+course_Prelim = courseList['Preliminary Enrollment']
+
+print(courseList.dtypes)
+
+
+# professorList = pd.read_csv("./dummyData/InstructorListTemplate.csv", dtype={0:'string', 1:'string', 2:'int'})
+# preferenceList = pd.read_csv("./dummyData/PreferenceList.csv")
+# timeList = pd.read_csv("./dummyData/TimeBlockTemplate.csv")
+# roomList = pd.read_csv("./dummyData/RoomListTemplate.csv")
+
+
