d4b674786c5f629481bf7dabb8d208e169c99310
Salwa Jeries <annette.jeries@gmail.com>
2024-03-06 17:10:25 -0600

    Defined period and day types

---
+++ frontend/src/app/_types/Period.ts
@@ -0,0 +1,23 @@
+/**
+ * Period
+ * @uuid Unique ID for course object;
+ * @start_time Start time for period;
+ * @end_time End time for period;
+ * @day Day enum for which day the period applies to;
+ */
+export type Period = {
+  uuid: string;
+  start_time: string;
+  end_time: string;
+  day: Day;
+};
+
+/**
+ * Day
+ * @MW - Monday/Wednesday class
+ * @TR - Tuesday/Thursday class
+ */
+enum Day {
+  MW = "MW",
+  TR = "TR",
+}
--- frontend/src/app/_types/TimeBlock.ts
+++
@@ -1,9 +0,0 @@
-/**
- * TimeBlock
- * @startTime - start of time block
- * @endTime - end of time block
- */
-export type TimeBlock = {
-  startTime: number;
-  endTime: number;
-};
