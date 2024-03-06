575a6341c0a6f725559fbbca9bdd8efe35fafc99
Salwa Jeries <annette.jeries@gmail.com>
2024-03-06 16:59:17 -0600

    Defined room type

--- frontend/src/app/_types/Room.ts
+++ frontend/src/app/_types/Room.ts
@@ -1,9 +1,11 @@
 /**
  * Room
- * @id - room code
- * @maxCapacity - maximum students that can fit in the room
+ * @uuid Unique ID for course object;
+ * @room_id Room ID with building code and room number;
+ * @max_capacity Maximum number of students allowed in the room;
  */
 export type Room = {
-  id: string;
-  maxCapacity: number;
+  uuid: string;
+  room_id: string;
+  max_capacity: string;
 };
