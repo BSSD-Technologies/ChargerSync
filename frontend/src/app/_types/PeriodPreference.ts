3d9b17b0d6af6e2ad06cf268d42cae3d9a49f4ca
Salwa Jeries <annette.jeries@gmail.com>
2024-03-07 14:24:03 -0600

    Defined period preference type

---
+++ frontend/src/app/_types/PeriodPreference.ts
@@ -0,0 +1,11 @@
+/**
+ * Period Preference
+ * @uuid Unique ID for period preference;
+ * @instructor_uuid unique ID to corresponding instructor;
+ * @course_uuid unique ID to corresponding period;
+ */
+export type PeriodPreference = {
+  uuid: string;
+  instructor_uuid: string;
+  period_uuid: string;
+};
