f55c78dd736b6ea398ea42c794cc2a6e96101065
Salwa Jeries <annette.jeries@gmail.com>
2024-03-18 16:36:41 -0500

    Started import course endpoint, check for csv file

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS  
 
 app = Flask(__name__)
@@ -7,6 +7,20 @@ CORS(app)  # Enable CORS for all routes
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/import/courses', methods=['POST'])
+def process_csv():
+    # Check if a file is present in the request
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+
+    file = request.files['file']
+
+    # Check if the file has a CSV extension
+    if not file.filename.endswith('.csv'):
+        return jsonify({'error': 'Invalid file format. Must be a CSV file'}), 400
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
