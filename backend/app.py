228f774ba6c1d68135d278ecc27b029dc4b6bfc3
Salwa Jeries <annette.jeries@gmail.com>
2024-03-18 17:24:06 -0500

    Implemented import courses endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS  
+from functions import *
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -8,7 +9,17 @@ CORS(app)  # Enable CORS for all routes
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/import/courses', methods=['POST'])
+"""
+/import/courses
+User uploads a course template .csv file to be parsed into JSON object.
+
+Error Codes:
+200 - OK
+400 - No file parameter was provided
+415 - File type is invalid (not a CSV)
+412 - File does not match expected template
+"""
+@app.route('/import/courses',  methods=['POST'])
 def process_csv():
     # Check if a file is present in the request
     if 'file' not in request.files:
@@ -18,9 +29,18 @@ def process_csv():
 
     # Check if the file has a CSV extension
     if not file.filename.endswith('.csv'):
-        return jsonify({'error': 'Invalid file format. Must be a CSV file'}), 400
+        return jsonify({'error': 'Invalid file format. Must be a CSV file'}), 415
     
-    
+    # Parse file from csv to JSON object
+    # Validate for course template headers
+    parseFile = csv_to_json(file, "course")
 
+    # Check if template was valid and return value
+    if (parseFile):
+        return parseFile, 200
+    else:
+        return jsonify({'error': "Invalid template."}), 412
+
+    
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
