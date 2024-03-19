7316bc101882bcb185c0eebe78172b2091611bee
Salwa Jeries <annette.jeries@gmail.com>
2024-03-19 12:06:27 -0500

    Wrote API endpoint for import rooms

--- backend/app.py
+++ backend/app.py
@@ -73,6 +73,37 @@ def process_rooms_csv():
     else:
         return jsonify({'error': "Invalid template."}), 412
 
+"""
+/import/periods
+User uploads a period template .csv file to be parsed into JSON object.
+
+Error Codes:
+200 - OK
+400 - No file parameter was provided
+415 - File type is invalid (not a CSV)
+412 - File does not match expected template
+"""
+@app.route('/import/periods',  methods=['POST'])
+def process_rooms_csv():
+    # Check if a file is present in the request
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+
+    file = request.files['file']
+
+    # Check if the file has a CSV extension
+    if not file.filename.endswith('.csv'):
+        return jsonify({'error': 'Invalid file format. Must be a CSV file'}), 415
+    
+    # Parse file from csv to JSON object
+    # Validate for period template headers
+    parseFile = csv_to_json(file, "period")
+
+    # Check if template was valid and return value
+    if (parseFile):
+        return parseFile, 200
+    else:
+        return jsonify({'error': "Invalid template."}), 412
     
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
