04e65ff9c1007b31b56c845e842f2289da0a5d22
Salwa Jeries <annette.jeries@gmail.com>
2024-03-30 17:28:54 -0500

    Removed my lil test button stuff

--- frontend/src/app/input-data/input-sections/submit-input.tsx
+++ frontend/src/app/input-data/input-sections/submit-input.tsx
@@ -4,35 +4,8 @@ import { Box, Grid, Stack, Typography } from "@mui/material";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import { LoadingButton } from "@mui/lab";
 import Link from "next/link";
-import {
-  useGlobalCourseListStore,
-  useGlobalInstructorListStore,
-  useGlobalPeriodListStore,
-  useGlobalPreferenceListStore,
-  useGlobalRoomListStore,
-} from "@/app/_stores/store";
 
 export default function SubmitInput() {
-  const [courseList] = [useGlobalCourseListStore((state) => state.courseList)];
-  const [roomList] = [useGlobalRoomListStore((state) => state.roomList)];
-  const [periodList] = [useGlobalPeriodListStore((state) => state.fullPeriodList)];
-  const [instructorList] = [
-    useGlobalInstructorListStore((state) => state.instructorList),
-  ];
-  const [coursePrefList, periodPrefList] = [
-    useGlobalPreferenceListStore((state) => state.coursePrefList),
-    useGlobalPreferenceListStore((state) => state.periodPrefList),
-  ];
-
-  const testFunction = () => {
-    console.log("Courses:", courseList);
-    console.log("Rooms:", roomList);
-    console.log("Periods:", periodList);
-    console.log("Instructors:", instructorList);
-    console.log("Course Prefs:", coursePrefList);
-    console.log("Period Prefs:", periodPrefList);
-  };
-
   return (
     <Box
       sx={{
@@ -47,7 +20,6 @@ export default function SubmitInput() {
             Click here to generate a schedule based on the data you provided.
           </Typography>
         </Stack>
-        <LoadingButton onClick={testFunction}>Test</LoadingButton>
         <Link passHref href="/schedule-report">
           <LoadingButton
             variant="contained"
