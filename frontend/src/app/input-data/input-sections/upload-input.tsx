4bf2e3e744c0810f75fe1ff91d7925870c4cbcff
Salwa Jeries <annette.jeries@gmail.com>
2024-03-30 22:40:07 -0500

    Created templates.zip for download, not pretty but it works

--- frontend/src/app/input-data/input-sections/upload-input.tsx
+++ frontend/src/app/input-data/input-sections/upload-input.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import {
-  Box,
-  Button,
-  Grid,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import Link from "next/link";
-import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
 
 export default function UploadInput() {
   const theme = useTheme();
@@ -22,20 +16,29 @@ export default function UploadInput() {
       }}
     >
       <Grid container alignItems={"center"} justifyContent={"space-between"}>
-        <Stack direction={"column"}>
-          <Typography variant="h5">Upload schedule data</Typography>
-          <Typography variant="body1">
-            Upload filled-out templates of schedule data for generating a
-            schedule.
-          </Typography>
-        </Stack>
-        <Link
-          passHref
-          href="/CourseListTemplate.csv"
-          download={"CourseListTemplate.csv"}
-        >
-          <Button variant="outlined" startIcon={<DownloadRoundedIcon sx={{ marginLeft: "5px" }} />}>Download Template</Button>
-        </Link>
+        <Grid item xs={8}>
+          <Stack direction={"column"}>
+            <Typography variant="h5">
+              Download course schedule input templates
+            </Typography>
+            <Typography variant="body1">
+              Download these CSV templates, fill out the course schedule
+              information for the respective input file, and upload to the
+              respective sections below to bulk upload your scheduling
+              information.
+            </Typography>
+          </Stack>
+        </Grid>
+        <Grid item justifyContent={"right"}>
+          <Link passHref href="/templates.zip" download={"templates.zip"}>
+            <Button
+              variant="outlined"
+              startIcon={<DownloadRoundedIcon sx={{ marginLeft: "5px" }} />}
+            >
+              Download Templates
+            </Button>
+          </Link>
+        </Grid>
       </Grid>
     </Box>
   );
