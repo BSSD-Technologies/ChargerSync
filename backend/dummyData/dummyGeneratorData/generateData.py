6289e20e4e08e838a11fbf91f89dacdffb4d8b9a
David Tollett <djtollett007@gmail.com>
2024-04-01 16:29:51 -0500

    Updated generateData.py to remove hyphens and duplicates from the Course template file

--- backend/dummyData/dummyGeneratorData/generateData.py
+++ backend/dummyData/dummyGeneratorData/generateData.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import json
+import csv
+from os import remove
+
+def read_csv_as_list(csv_file):
+    data = []
+    with open(csv_file, 'r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            data.append(row)
+    return data
 
 def json_to_csv(json_filename, csv_mapping):
     with open(json_filename, 'r') as file:
@@ -23,5 +33,50 @@ csv_mapping = {
     'roomTemplate.csv': ['building']
 }
 
+def removeCourseHyphensAndDuplicates(csv_filename):
+    csv_data = read_csv_as_list(csv_filename)
+    j = 0
+    for i in csv_data:
+        if j != 0:        
+            temp = i[1]
+            temp2 = temp[:3]
+            csv_data[j][1] = temp2
+            j+=1
+        else:
+            j+=1
+    csv_data.pop(0)
+
+    temparr = []
+    results = []
+    j = 0
+    for i in csv_data:
+        temp = i [0]
+        temp2 = i[1]
+        temp_str = i[0] + ' ' + i[1]
+
+        if j!=0:
+            if temp_str in temparr:
+                results.append(i)
+            else:
+                temparr.append(temp_str)
+            j+=1
+        else:
+            temparr.append(temp_str)
+            j+=1
+
+    for i in results:
+        csv_data.remove(i)
+
+    print(csv_data)
+    remove(csv_filename)
+    myFile = open(csv_filename, 'w')
+    writer = csv.writer(myFile)
+    writer.writerow(['department', 'course', 'max_enrollment', 'enrollment'])
+    for data_list in csv_data:
+        writer.writerow(data_list)
+    myFile.close()
+
+
 # Convert JSON data to CSV
 json_to_csv("rawCourseData.json", csv_mapping)
+removeCourseHyphensAndDuplicates('courseTemplate.csv')
