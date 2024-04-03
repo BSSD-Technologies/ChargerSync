021f99dd1677c19576f6566afc75f760b661ed80
thebamorgan <77521759+thebamorgan@users.noreply.github.com>
2024-04-03 15:43:28 -0500

    Reworked generateData.py to clean it up and make it smoother

--- backend/dummyData/generateData.py
+++ backend/dummyData/generateData.py
@@ -1,9 +1,4 @@
-import pandas as pd
-import json
-import csv
-import random
-from os import remove
-from functools import reduce
+import json, csv, random, pandas as pd
 
 def read_csv_as_list(csv_file):
     data = []
@@ -20,121 +15,76 @@ def json_to_csv(json_filename, csv_mapping):
     for csv_filename, columns in csv_mapping.items():
         # Convert JSON data into a DataFrame
         df = pd.DataFrame(json_data).sample(n=100)
-        
-        if csv_filename == 'roomTemplate.csv':
-            # Filter out rows where 'building' is 'ONLN' or 'TBA'
-            df = df[~df['building'].isin(['ONLN', 'TBA', 'REMT', 'A&M', 'ONLINE'])]
-            
-            # Combine 'building' and 'room' to create 'room_id'
-            df['room_id'] = df['building'] + '-' + df['room']
-            
-            # Sort DataFrame by 'room_id'
-            df.sort_values(by='room_id', inplace=True)
 
-        elif csv_filename == 'periodTemplate.csv':
-            df = df[~df['start'].isin(['TBA'])]
-            df.drop_duplicates(subset=['start', 'end'], inplace=True)
-            df.sort_values(by='start', inplace=True)
+
+        if csv_filename == 'courseTemplate.csv':
+            # Combine logic to remove hyphens and duplicates
+            df['course_number'] = df['course'].apply(lambda x: x[:3])  # Rename 'course' to 'course_number'
+            df.drop_duplicates(subset=['department', 'course_number'], inplace=True)  # Update to 'course_number'
+            df.rename(columns={'enrollment': 'preliminary_enrollment'}, inplace=True)
+            df = df[['department', 'course_number', 'max_enrollment', 'preliminary_enrollment']]
+
+
+
+
 
         elif csv_filename == 'instructorTemplate.csv':
             # Remove duplicates based on 'instructor' column
             df.drop_duplicates(subset=['instructor'], inplace=True)
             
             # Split 'instructor' column into 'first_name' and 'last_name'
-            df[['First Name', 'Last Name']] = df['instructor'].str.split(n=1, expand=True)
+            df[['first_name', 'last_name']] = df['instructor'].str.split(n=1, expand=True)
             
-            # Select only the required columns
-            df = df[['First Name', 'Last Name']]
-        
-        # Select only the required columns
-        df = df[columns]
-        
-        if csv_filename == 'roomTemplate.csv':
+            # Filter out rows where 'first_name' or 'last_name' is 'STAFF'
+            df = df[(df['first_name'] != 'STAFF') & (df['last_name'] != 'STAFF')]
+            
+            # Add random priority column
+            df['priority'] = [random.randint(1, 10) for _ in range(len(df))]
+
+            df = df[['first_name', 'last_name', 'priority']]
+
+
+
+
+        elif csv_filename == 'roomTemplate.csv':
+            # Filter out rows where 'building' is 'ONLN' or 'TBA'
+            df = df[~df['building'].isin(['ONLN', 'TBA', 'REMT', 'A&M', 'ONLINE'])]
+            
+            # Combine 'building' and 'room' to create 'room_id'
+            df['room_id'] = df['building'] + '-' + df['room']
+
             min_capacity = 30
             max_capacity = 150
             capacity_increment = 10
-            df['Max Capacity'] = [random.randrange(min_capacity, max_capacity+1, capacity_increment) for _ in range(len(df))]
-        
+            df['max_capacity'] = [random.randrange(min_capacity, max_capacity+1, capacity_increment) for _ in range(len(df))]
+            
+            # Sort DataFrame by 'room_id'
+            df.sort_values(by='room_id', inplace=True)
+
+            df = df[['room_id', 'max_capacity']]
+
+
+
+        elif csv_filename == 'periodTemplate.csv':
+            df = df[~df['start'].isin(['TBA'])]
+            df.drop_duplicates(subset=['start', 'end'], inplace=True)
+            df.sort_values(by='start', inplace=True)
+            df.rename(columns={'start': 'start_time', 'end': 'end_time'}, inplace=True)
+            df = df[['start_time', 'end_time']]
+
+
+        # Select only the required columns
+        df = df[columns]
         # Write DataFrame to CSV file
         df.to_csv(csv_filename, index=False)
 
 # Define CSV mappings
 csv_mapping = {
-    'courseTemplate.csv': ['department', 'course', 'max_enrollment', 'enrollment'],
-    'instructorTemplate.csv': ['First Name', 'Last Name'],
-    'periodTemplate.csv': ['start', 'end'],
-    'roomTemplate.csv': ['room_id']
+    'courseTemplate.csv': ['department', 'course_number', 'max_enrollment', 'preliminary_enrollment'],
+    'instructorTemplate.csv': ['first_name', 'last_name', 'priority'],
+    'periodTemplate.csv': ['start_time', 'end_time'],
+    'roomTemplate.csv': ['room_id', 'max_capacity']
 }
-def removeCourseHyphensAndDuplicates(csv_filename):
-    csv_data = read_csv_as_list(csv_filename)
-    j = 0
-    for i in csv_data:
-        if j != 0:        
-            temp = i[1]
-            temp2 = temp[:3]
-            csv_data[j][1] = temp2
-            j+=1
-        else:
-            j+=1
-    csv_data.pop(0)
-
-    temparr = []
-    results = []
-    j = 0
-    for i in csv_data:
-        temp = i [0]
-        temp2 = i[1]
-        temp_str = i[0] + ' ' + i[1]
-
-        if j!=0:
-            if temp_str in temparr:
-                results.append(i)
-            else:
-                temparr.append(temp_str)
-            j+=1
-        else:
-            temparr.append(temp_str)
-            j+=1
-
-    for i in results:
-        csv_data.remove(i)
-
-    # print(csv_data)
-    remove(csv_filename)
-    myFile = open(csv_filename, 'w')
-    writer = csv.writer(myFile)
-    writer.writerow(['Department', 'Course Number', 'Max Enrollment', 'Preliminary Enrollment'])
-    for data_list in csv_data:
-        writer.writerow(data_list)
-    myFile.close()
-
-def fixNameIssues(csv_filename):
-    csv_data = read_csv_as_list(csv_filename)  # Assuming read_csv_as_list is defined elsewhere
-    temparr = []
-    unique_names = set()  # Store unique names to eliminate duplicates
-    for i, row in enumerate(csv_data):
-        if i != 0:  # Skip header
-            full_name = ' '.join(row)  # Combine all parts of the name
-            name_parts = full_name.split()
-            last_name = name_parts[-1]
-            if last_name != "STAFF":
-                first_name = ' '.join(name_parts[:-1])
-                name = (first_name, last_name)  # Tuple to represent the name
-                if name not in unique_names:  # Check if the name is unique
-                    unique_names.add(name)
-                    temparr.append([first_name, last_name])
-            else:
-                continue
-
-    # Write to the new CSV file
-    with open("instructorTemplate.csv", 'w', newline='') as myFile:
-        writer = csv.writer(myFile)
-        writer.writerow(['First Name', 'Last Name'])
-        writer.writerows(temparr)
-
-    return
 
 # Convert JSON data to CSV
 json_to_csv("rawCourseData.json", csv_mapping)
-removeCourseHyphensAndDuplicates('courseTemplate.csv')
-fixNameIssues('instructorTemplate.csv')
