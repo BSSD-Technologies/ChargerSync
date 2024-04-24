60254ab24613c5aeb84ce4180d2966f30d20f7ca
thebamorgan <77521759+thebamorgan@users.noreply.github.com>
2024-04-24 16:48:44 -0500

    fixed casing

--- frontend/src/app/help/page.tsx
+++ frontend/src/app/help/page.tsx
@@ -13,7 +13,7 @@ export default function HelpManual() {
     >
       <Typography variant="h4">Generating a schedule by uploading a template file.</Typography>
       <div>
-        <iframe src="https://scribehow.com/embed/Generate_a_schedule_using_the_file_upload_method___zekdW9XQxKDHy2pxF7kTg?skipIntro=true" width="100%" height="640" allowfullscreen frameborder="0"></iframe>
+        <iframe src="https://scribehow.com/embed/Generate_a_schedule_using_the_file_upload_method___zekdW9XQxKDHy2pxF7kTg?skipIntro=true" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
       </div>
     </Container>
   );
