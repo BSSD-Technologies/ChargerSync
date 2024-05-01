5e8f926e2243c7a6b1e541006819f3e92a1dfab4
thebamorgan <77521759+thebamorgan@users.noreply.github.com>
2024-04-30 21:39:26 -0500

    Updated the help manual page with relevant guides

--- frontend/src/app/help/page.tsx
+++ frontend/src/app/help/page.tsx
@@ -17,35 +17,35 @@ export default function HelpManual() {
         className="help-style"
       >
         <Typography variant="h4">Generating a schedule by uploading a template file.</Typography>
-        <iframe src="https://scribehow.com/embed/Basic_Runthrough__DYAlNZjmQDinRoKNuojvOw?skipIntro=true" width="100%" height="640" allowfullscreen frameborder="0"></iframe>
+        <iframe src="https://scribehow.com/embed/Basic_Runthrough__DYAlNZjmQDinRoKNuojvOw?skipIntro=true" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
       </div>
 
       <div
         className = "help-style"
       >
         <Typography variant="h4">Generating a schedule by manually entering data.</Typography>
-        {/* replace with iframe */}
+        <iframe src="https://scribehow.com/embed/Generating_a_schedule_by_manually_entering_data__OsCR8pfLSL6Xh0D6JzKojQ?skipIntro=true" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
       </div>
 
       <div
         className = "help-style"
       >
         <Typography variant="h4">Downloading your currently entered data.</Typography>
-        {/* replace with iframe */}
+        <iframe src="https://scribehow.com/embed/Downloading_your_currently_entered_data__sIqspiycQD-Y1feFFSdKAg?skipIntro=true" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
       </div>
 
       <div
         className = "help-style"
       >
-        <Typography variant="h4">Sorting and filtering schedule results</Typography>
-        {/* replace with iframe */}
+        <Typography variant="h4">Filtering schedule results.</Typography>
+        <iframe src="https://scribehow.com/embed/Sorting_and_filtering_schedule_results__SCTKJrWASl29PkoSCdz1Xw?skipIntro=true" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
       </div>
 
       <div
         className = "help-style"
       >
-        <Typography variant="h4">Exporting results.</Typography>
-        {/* replace with iframe */}
+        <Typography variant="h4">How to export results.</Typography>
+        <iframe src="https://scribehow.com/embed/Exporting_a_schedule__-mDblMv_Tb2DY1HNYShO_g?skipIntro=true" width="100%" height="640" allowFullScreen frameBorder="0"></iframe>
       </div>
     </Container>
   );
